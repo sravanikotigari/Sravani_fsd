c4806ffe238f2a7b0b3f6b0cdaccb36a73cc99da
sravanikotigari <59565038+sravanikotigari@users.noreply.github.com>
2020-01-27 09:20:01 +0530

    Add files via upload

--- String.java
+++ String.java
@@ -1,39 +1,9 @@
-import java.util.Scanner;
-public class String {
-               public static void main(String[] args) {
-		        int count;
-		        String temp;
-		        Scanner scan = new Scanner(System.in);
-		        System.out.print("Enter number of strings you would like to enter:");
-		        count = scan.nextInt();
-		        String str[] = new String[count];
-		        Scanner scan2 = new Scanner(System.in);
-		        System.out.println("Enter the Strings one by one:");
-		        for(int i = 0; i < count; i++)
-		        {
-		            str[i] = scan.nextLine();
-		        }
-		        scan.close();
-		        scan2.close();
-		      for (int i = 0; i < count; i++) 
-		        {
-		            for (int j = i + 1; j < count; j++) 
-		            { 
-		                if (str[i].compareTo(str[j])>0) 
-		                {
-		                    temp = str[i];
-		                    str[i] = str[j];
-		                    str[j] = temp;
-		                }
-		            }
-		        }
-		        System.out.print("Strings in Sorted Order:");
-		        for (int i = 0; i <= count - 1; i++) 
-		        {
-		            System.out.print(str[i] + ", ");
-		        }
-		    }
-		
- }
-
-
+public class String{  
+public static void main(String args[]){  
+String strMain ="Welcome to java"; 
+String[] arrSplit_3 = strMain.split("\\s");
+    for (int i=0; i < arrSplit_3.length; i++){
+      System.out.println(arrSplit_3[i]);
+    }
+  }
+}
